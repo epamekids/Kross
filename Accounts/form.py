30151cdbb9fe715b5b524573a368c075c7bb735b
Michael Shkarubski <40541820+epamekids@users.noreply.github.com>
2018-10-10 18:34:20 +0300

    fixing syntax errors

--- Accounts/form.py
+++ Accounts/form.py
@@ -1,5 +1,5 @@
 class Registration(forms.Form):
     nickname = forms.CharField(label = "Your Nickname",required=True)
-    firstname = forms.CharField(label ="First Name",required=True)
-    lastname = forms.CharField(label ="Last Name",required=True)
+    firstname = forms.CharField(label ="First Name")
+    lastname = forms.CharField(label ="Last Name")
     email = forms.EmailField(label ="Your Email Address",required=True)
