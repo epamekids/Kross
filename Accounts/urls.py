101ab5907902637d142a07719cc1f6033b2ed3cd
Michael Shkarubski <40541820+epamekids@users.noreply.github.com>
2018-10-13 11:57:08 +0300

    aaa

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login, name="login"),
-    path("signup/", views.signup, name="signup")
-    path("logout/", views.logout, name="logout")
+    path("signup/", views.signup, name="signup"),
    ]
