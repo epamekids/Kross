ea49d4226cf2715b706274febc6665b9b0206007
Michael Shkarubski <40541820+epamekids@users.noreply.github.com>
2018-10-20 20:45:32 +0300

    ффф

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class SignUp(forms.Form):
     nickname = forms.CharField(label = "Your nickname",required=True)
     password = forms.CharField(label = "Your password",required=True)
--- Accounts/views.py
+++ Accounts/views.py
@@ -4,21 +4,12 @@ from Accounts.forms import SignUp, LogIn
 from django.http import HttpResponse
 from Accounts.models import User
 
-def login(request):
-  try:
-      member = request.session.get(username=request.POST['username'])
-      if member.password == request.POST['password']:
-          request.session['member_id'] = member.id
-          return HttpResponse("You're signed in.")
-  except User.DoesNotExist:
-      return HttpResponse("Your nickname or password is incorrect. Try again.")
-
 def signup(request):
     if request.method == 'POST':
         form = SignUp(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/register')
+            return redirect('/')
     else:
         form = SignUp()
     return render(request, 'registration/signUp.html', {'form': SignUp()})
