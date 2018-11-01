d746fc3e3bda0fd42d883799c7884ac980b6d17a
Michael Shkarubski <40541820+epamekids@users.noreply.github.com>
2018-11-01 16:39:37 +0300

    ADding profile

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from Accounts.forms import SignUp
 
 
 def profile(request):
